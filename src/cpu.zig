14d8607772caa70e1a0262d07b4643878a458580
Mano Sriram <mano.sriram0@gmail.com>
2025-06-14 01:54:14 +0530

    Add instruction pointer and update new func

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 const RegisterPool = struct {
     A: u8 = 0,
@@ -15,16 +16,30 @@ const Flags = packed struct {
 
 pub const CPU = struct {
     Register: RegisterPool = .{},
-    Memory: [256]u8 = std.mem.zeroes([256]u8),
-    InstructionPointer: u8 = 0,
+    Memory: [256]u8,
+    InstructionPointer: ?*u8,
     Flags: Flags = .{},
     Stack: std.ArrayList(u8),
-    Allocator: std.mem.Allocator,
+    StackPtr: ?*u8 = null,
+    Allocator: Allocator,
 
-    pub fn new(allocator: std.mem.Allocator) CPU {
+    pub fn new(allocator: Allocator) CPU {
+        const memory = std.mem.zeroes([256]u8);
         return .{
             .Stack = std.ArrayList(u8).init(allocator),
             .Allocator = allocator,
+            .InstructionPointer = null,
+            .Memory = memory,
         };
+    }
+
+    pub fn push(self: *CPU, item: u8) !void {
+        try self.Stack.append(item);
+        self.StackPtr = if (self.Stack.items.len > 0) &self.Stack.items[self.Stack.items.len - 1] else null;
+    }
+
+    pub fn pop(self: *CPU) !void {
+        _ = self.Stack.pop();
+        self.StackPtr = if (self.Stack.items.len > 0) &self.Stack.items[self.Stack.items.len - 1] else null;
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,21 @@ const std = @import("std");
 const cpu = @import("cpu.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer _ = gpa.deinit();
+    // const allocator = gpa.allocator();
 
-    var c = cpu.CPU.new(allocator);
-    defer c.Stack.deinit();
 
-    try c.Stack.append(123);
+    const file = try std.fs.cwd().openFile("./source.asm", .{});
+    defer file.close();
 
-    std.debug.print("{} {}", .{c.Memory[0], c.Stack.items.len});
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    var buf: [1024]u8 = undefined;
+
+    // TODO: parse line and handle opcodes
+    while (try in_stream.readUntilDelimiterOrEof(buf[0..], '\n')) |line| {
+        std.debug.print("{s}\n", .{line});
+    }
 }
